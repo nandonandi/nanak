b98c6a7238c52dbe366e6ab66b96d162f0ca0a0d
nandonandi <nandoonandi5@gmail.com>
2025-05-24 14:08:54 -0700

    Update nanak18.py

--- nanak18.py
+++ nanak18.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://30f229fee2c14d9ad3c4__cr.sg:abb79949fe6ee6ad@gw.dataimpulse.com:10018',
-        'https': 'https://30f229fee2c14d9ad3c4__cr.sg:abb79949fe6ee6ad@gw.dataimpulse.com:10018',
+        'http': 'http://30f229fee2c14d9ad3c4__cr.it:abb79949fe6ee6ad@gw.dataimpulse.com:10018',
+        'https': 'https://30f229fee2c14d9ad3c4__cr.it:abb79949fe6ee6ad@gw.dataimpulse.com:10018',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
