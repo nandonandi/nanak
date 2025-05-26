879b70e65deae3ff7379f0a58748409b4fba632d
nandonandi <nandoonandi5@gmail.com>
2025-05-25 22:42:32 -0700

    Update nanak12.py

--- nanak12.py
+++ nanak12.py
@@ -31,7 +31,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/3bcf489a-f3c7-492c-9cd2-ba936972f800")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/3d67cda3-3a68-4d0c-b40f-b38e995008a5")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
