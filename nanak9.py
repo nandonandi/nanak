4ab21f4c1ba5b06c7d4e6bcbd9033cae48e7c969
nandonandi <nandoonandi5@gmail.com>
2025-05-31 13:10:38 -0700

    Update nanak9.py

--- nanak9.py
+++ nanak9.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://0e31ebbc602a0282461f__cr.au:56104e674f0b518c@gw.dataimpulse.com:10009',
-        'https': 'https://0e31ebbc602a0282461f__cr.au:56104e674f0b518c@gw.dataimpulse.com:10009',
+        'http': 'http://ee41635569d88a2d10da__cr.au:b52b3f11ffa4eb70@gw.dataimpulse.com:10009',
+        'https': 'https://ee41635569d88a2d10da__cr.au:b52b3f11ffa4eb70@gw.dataimpulse.com:10009',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
