617fafb7c03917cd06c1a612ea958a195995f57c
nandonandi <nandoonandi5@gmail.com>
2025-06-04 23:22:59 -0700

    Update nanak4.py

--- nanak4.py
+++ nanak4.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://ee41635569d88a2d10da__cr.cl:b52b3f11ffa4eb70@gw.dataimpulse.com:10004',
-        'https': 'https://ee41635569d88a2d10da__cr.cl:b52b3f11ffa4eb70@gw.dataimpulse.com:10004',
+        'http': 'http://ab111cd9a958a7a8cfef__cr.cl:b965870c1d9f7d46@gw.dataimpulse.com:10004',
+        'https': 'https://ab111cd9a958a7a8cfef__cr.cl:b965870c1d9f7d46@gw.dataimpulse.com:10004',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
