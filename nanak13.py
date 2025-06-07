e7fd8ad70454e13cab706e5597446368fe7b01f4
nandonandi <nandoonandi5@gmail.com>
2025-06-06 19:40:51 -0700

    Update nanak13.py

--- nanak13.py
+++ nanak13.py
@@ -47,7 +47,7 @@ chrome_options.add_argument("--disable-blink-features=AutomationControlled") # M
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/d697de09-6731-40da-af6f-167b06addefd")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/57ef6149-ff8f-4fb7-a50a-5d8a9eb2d360")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
