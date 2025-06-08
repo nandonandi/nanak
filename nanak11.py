8ab0b430a4268f0fd0e9e7bde7b80dbcb0a7d717
nandonandi <nandoonandi5@gmail.com>
2025-06-08 16:38:35 -0700

    Update nanak11.py

--- nanak11.py
+++ nanak11.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://ab111cd9a958a7a8cfef__cr.Pl:b965870c1d9f7d46@gw.dataimpulse.com:10111',
-        'https': 'https://ab111cd9a958a7a8cfef__cr.Pl:b965870c1d9f7d46@gw.dataimpulse.com:10111',
+        'http': 'http://ab111cd9a958a7a8cfef__cr.Pl:b965870c1d9f7d46@gw.dataimpulse.com:10101',
+        'https': 'https://ab111cd9a958a7a8cfef__cr.Pl:b965870c1d9f7d46@gw.dataimpulse.com:10101',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
