99f438921497a49796f72a84bde51e88b99d4ba5
nandonandi <nandoonandi5@gmail.com>
2025-06-09 06:39:55 -0700

    Update nanak16.py

--- nanak16.py
+++ nanak16.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://ab111cd9a958a7a8cfef__cr.tr:b965870c1d9f7d46@gw.dataimpulse.com:10116',
-        'https': 'https://ab111cd9a958a7a8cfef__cr.tr:b965870c1d9f7d46@gw.dataimpulse.com:10116',
+        'http': 'http://23c87754edd92021cd4d__cr.tr:ed7dbde64768ae8a@gw.dataimpulse.com:10116',
+        'https': 'https://23c87754edd92021cd4d__cr.tr:ed7dbde64768ae8a@gw.dataimpulse.com:10116',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
